fee7ccd0ea5aedceb3a008e4c52e37e811c2ac6c
Crowdbotics <team@crowdbotics.com>
2021-09-08 16:17:10 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,6 @@
+export const globalOptions = {
+  name: "dry_art_30391Identifier",
+  url: "https://dry_art_30391Identifier.botics.co",
+  api: "https://dry_art_30391Identifier.botics.co/api/v1"
+}
+export const modulesOptions = {}
